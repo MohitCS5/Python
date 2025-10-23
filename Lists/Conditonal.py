cbd542fc36c67f3f923989423d4361a1269100c3
mohit <thisismohitstm@gmail.com>
2025-10-23 20:57:44 +0530

    Lists

---
+++ Lists/Conditonal.py
@@ -0,0 +1,20 @@
+age= int(input("Enter you age "))
+income= int(input("Enter you income "))
+
+if(age>=18 or income>=10):
+    print("you are eligible ")
+else:
+    print("you are not eligible")
+
+a = False
+b = True
+if not a:
+    print("this will print")
+else:
+    print("print this")
+
+if not b:
+    print("this will print")
+else:
+    print("print this") 
+
