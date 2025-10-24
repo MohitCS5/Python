8144d293366ea05358645766d8524dd1e75c99e2
mohit <thisismohitstm@gmail.com>
2025-10-24 12:39:32 +0530

    Lists/01_lists.py

--- Lists/01_lists.py
+++ Lists/01_lists.py
@@ -1,2 +1,16 @@
 thisislist = [1, 2, 3, 4, 5]
 print(thisislist)
+
+#getting type of list
+mylist = ["apple ", "banana", "cherry"]
+print (type(mylist))
+
+#Remove list items
+a = ["12","apple","banana","mango"]
+a.remove("banana")
+print(len(a))
+
+#pop method
+a = ["12","apple","banana","mango"]
+a.pop(1)
+print(len(a))
