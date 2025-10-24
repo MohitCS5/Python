4670428d153ad340cbd4745cedb6d72eb4328712
mohit <thisismohitstm@gmail.com>
2025-10-24 18:47:23 +0530

    .\Lists\02_editing_lists.py

--- Lists/02_editing_lists.py
+++ Lists/02_editing_lists.py
@@ -13,3 +13,31 @@ print(a)
 a = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
 a[1:3] = ["blackcurrant", "watermelon"]
 print(a)
+
+a = ["apple", "banana", "cherry"]
+a[1:3] = ["watermelon"]
+print(a)
+
+b = ["apple", "banana", "cherry"]
+b.insert(2,"watermelon")
+print(b)
+
+b = ["apple", "banana", "cherry"]
+b.append("orange")
+print(b)
+
+c = ["apple", "banana", "cherry"]
+d = ["orange", "kiwi", "melon"]
+c.extend(d)
+print(c)
+
+#clear method
+c = ["apple", "banana", "cherry"]
+del c[1]
+print(c)
+
+c = ["apple", "banana", "cherry"]
+c.remove("banana")
+
+#in remove we specify the value name but in pop we have to 
+#specify the index value so use accordingly to your need
