8bfb12fcd4c2c219a3d24ee917673d52c63ad0ae
mohit <thisismohitstm@gmail.com>
2025-10-25 19:45:14 +0530

    Lists/05_sortinglist.py

---
+++ Lists/05_sortinglist.py
@@ -0,0 +1,8 @@
+thislist = ["watermelon", "banana", "orange", "kiwi", "mango"]
+thislist.sort()
+print(thislist)
+
+#numerical sort
+numlist =[100, 50, 65, 82, 23   ]
+numlist.sort()
+print(numlist)
