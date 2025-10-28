ac8c1e44296f0b235116f39a2cc67a452b5305d3
mohit <thisismohitstm@gmail.com>
2025-10-28 16:08:49 +0530

    Sets/acc.py

---
+++ Sets/acc.py
@@ -0,0 +1,37 @@
+#one cannot access items in set by reffereing to index or key since set dont have any index
+#but we can check if the value exist by using in keyword
+x = {"apple","mango","banana","cherry"}
+for x in x:
+    print(x)
+
+x = {"apple","mango","banana","cherry"}
+print("apple" in x )
+#not in to check if the value is not present in the variable x
+
+#we cannot replace change items in set but we can add one 
+
+x = {"apple","mango","banana","cherry"}
+y = {"red","yellow","yellow","greed"}
+x.update(y)
+print(x)
+
+#methods in set
+#1.add
+#2.update
+#3.remove and discard
+#4.pop and .clear
+
+x = {"apple","mango","banana","cherry"}
+x.remove("apple")
+print(x)
+
+#loop through
+x = {"apple","mango","banana","cherry"}
+for x in x:
+    print(x)
+
+x = {"apple","mango","banana","cherry"}
+i = 0
+while i<len(x):
+    print(x)
+    i =i+1
---
+++ Sets/join.py
@@ -0,0 +1,67 @@
+'''join methods
+union adn update() = join all items from both sides
+intersection() = keeps only duplicates
+differnence() = keeps the items from the first set that are not in otherset
+symmeteric_difference() = keeps all items except the duplicates'''
+
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+set3= set1.union(set2)
+print(set3)
+
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+set3 = set1.intersection(set2)
+print(set3)
+
+set1 = {1,2,3,5}
+set2 = {"apple","banana","cherry"}
+set3 = {"John", "Elena"}
+set4 = {"apple", "bananas", "cherry"}
+set5 = set1.union(set2,set3,set4)
+print(set5)
+
+set1 = {1,2,3,5}
+set2 = {"apple","banana","cherry"}
+set3 = {"John", "Elena"}
+set4 = {"apple", "bananas", "cherry"}
+myset = (set1 | set2 | set3 | set4)
+print(myset)
+
+# | this works same as union is the operator or union
+
+set2 = {"apple","banana","cherry"}
+set1 = ("apple","bananna","cherry")
+set3 = set2.union(set1)
+print(set3)
+
+#union doesnt change the original set it creates new if youwant to save the
+'''unions does not change the origingal set it returs a new set
+if you want to save changes in existing set use updat()
+otherwise use union but will have to create a new variable to storre the value
+'''
+set1 = {"a", "b" , "c"}
+set2 = {1, 2, 3}
+set1.update(set2)
+print(set1)
+
+#intersection returns a new set 
+#keep only duplicates
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set3 = set1.intersection(set2)
+print(set3)
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {"google", "microsoft", "apple"}
+set1.intersection_update(set2)
+print(set1)
+
+#frozenset
+set1 = frozenset({"apple", "banana", "cherry"})
+print(type(set1))
+
+
+set1 = {"apple", "banana", "cherry"}
+x = set1.copy(set1)
+print(x)
